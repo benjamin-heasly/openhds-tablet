2f3f06993dc40d4b2e86d43f172a548ef0e57a2d
Ben Heasly <benjamin.heasly@gmail.com>
2014-08-07 11:33:05 -0400

    basic relationship gateway and tests

---
+++ application/src/main/java/org/openhds/mobile/repository/RelationshipGateway.java
@@ -0,0 +1,58 @@
+package org.openhds.mobile.repository;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import org.openhds.mobile.OpenHDS;
+import org.openhds.mobile.model.Relationship;
+
+import static org.openhds.mobile.OpenHDS.Relationships.COLUMN_RELATIONSHIP_INDIVIDUAL_A;
+import static org.openhds.mobile.OpenHDS.Relationships.COLUMN_RELATIONSHIP_INDIVIDUAL_B;
+import static org.openhds.mobile.OpenHDS.Relationships.COLUMN_RELATIONSHIP_STARTDATE;
+import static org.openhds.mobile.OpenHDS.Relationships.COLUMN_RELATIONSHIP_TYPE;
+import static org.openhds.mobile.repository.RepositoryUtils.extractString;
+
+
+/**
+ * Convert Relationship to and from database.  Relationship-specific queries.
+ *
+ * Uses the extId of individualA as the id for the Relationship.
+ *
+ */
+public class RelationshipGateway extends Gateway<Relationship> {
+
+    public RelationshipGateway() {
+        super(OpenHDS.Relationships.CONTENT_ID_URI_BASE, COLUMN_RELATIONSHIP_INDIVIDUAL_A, new RelationshipConverter());
+    }
+
+    private static class RelationshipConverter implements Converter<Relationship> {
+
+        @Override
+        public Relationship fromCursor(Cursor cursor) {
+            Relationship relationship = new Relationship();
+
+            relationship.setIndividualA(extractString(cursor, COLUMN_RELATIONSHIP_INDIVIDUAL_A));
+            relationship.setIndividualB(extractString(cursor, COLUMN_RELATIONSHIP_INDIVIDUAL_B));
+            relationship.setStartDate(extractString(cursor, COLUMN_RELATIONSHIP_STARTDATE));
+            relationship.setType(extractString(cursor, COLUMN_RELATIONSHIP_TYPE));
+
+            return relationship;
+        }
+
+        @Override
+        public ContentValues toContentValues(Relationship relationship) {
+            ContentValues contentValues = new ContentValues();
+
+            contentValues.put(COLUMN_RELATIONSHIP_INDIVIDUAL_A, relationship.getIndividualA());
+            contentValues.put(COLUMN_RELATIONSHIP_INDIVIDUAL_B, relationship.getIndividualB());
+            contentValues.put(COLUMN_RELATIONSHIP_STARTDATE, relationship.getStartDate());
+            contentValues.put(COLUMN_RELATIONSHIP_TYPE, relationship.getType());
+
+            return contentValues;
+        }
+
+        @Override
+        public String getId(Relationship relationship) {
+            return relationship.getIndividualA();
+        }
+    }
+}
---
+++ instrumentation-tests/src/org/openhds/mobile/tests/RelationshipGatewayTest.java
@@ -0,0 +1,27 @@
+package org.openhds.mobile.tests;
+
+import org.openhds.mobile.model.Relationship;
+import org.openhds.mobile.repository.RelationshipGateway;
+
+
+public class RelationshipGatewayTest extends GatewayTest<Relationship> {
+
+    private RelationshipGateway relationshipGateway;
+
+    public RelationshipGatewayTest() {
+        super(new RelationshipGateway());
+        this.relationshipGateway = (RelationshipGateway) this.gateway;
+    }
+
+    @Override
+    protected Relationship makeTestEntity(String id, String name) {
+        Relationship relationship = new Relationship();
+
+        relationship.setIndividualA(id);
+        relationship.setIndividualB("INDIVIDUALB");
+        relationship.setStartDate("2000-01-01 00:00:00");
+        relationship.setType("TYPE");
+
+        return relationship;
+    }
+}
