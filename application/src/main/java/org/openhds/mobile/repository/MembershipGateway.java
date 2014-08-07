46898f586b05eeb6fb39e9109b8ef74192f851c8
Ben Heasly <benjamin.heasly@gmail.com>
2014-08-07 12:50:11 -0400

    basic gateway and tests for memberships

---
+++ application/src/main/java/org/openhds/mobile/repository/MembershipGateway.java
@@ -0,0 +1,55 @@
+package org.openhds.mobile.repository;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import org.openhds.mobile.OpenHDS;
+import org.openhds.mobile.model.Membership;
+
+import static org.openhds.mobile.OpenHDS.Memberships.COLUMN_INDIVIDUAL_EXTID;
+import static org.openhds.mobile.OpenHDS.Memberships.COLUMN_MEMBERSHIP_RELATIONSHIP_TO_HEAD;
+import static org.openhds.mobile.OpenHDS.Memberships.COLUMN_SOCIAL_GROUP_EXTID;
+import static org.openhds.mobile.repository.RepositoryUtils.extractString;
+
+
+/**
+ * Convert Membership to and from database.  Membership-specific queries.
+ *
+ * Uses the extId of the individual as the id for the Membership.
+ *
+ */
+public class MembershipGateway extends Gateway<Membership> {
+
+    public MembershipGateway() {
+        super(OpenHDS.Memberships.CONTENT_ID_URI_BASE, COLUMN_INDIVIDUAL_EXTID, new MembershipConverter());
+    }
+
+    private static class MembershipConverter implements Converter<Membership> {
+
+        @Override
+        public Membership fromCursor(Cursor cursor) {
+            Membership membership = new Membership();
+
+            membership.setIndividualExtId(extractString(cursor, COLUMN_INDIVIDUAL_EXTID));
+            membership.setSocialGroupExtId(extractString(cursor, COLUMN_SOCIAL_GROUP_EXTID));
+            membership.setRelationshipToHead(extractString(cursor, COLUMN_MEMBERSHIP_RELATIONSHIP_TO_HEAD));
+
+            return membership;
+        }
+
+        @Override
+        public ContentValues toContentValues(Membership membership) {
+            ContentValues contentValues = new ContentValues();
+
+            contentValues.put(COLUMN_INDIVIDUAL_EXTID, membership.getIndividualExtId());
+            contentValues.put(COLUMN_SOCIAL_GROUP_EXTID, membership.getSocialGroupExtId());
+            contentValues.put(COLUMN_MEMBERSHIP_RELATIONSHIP_TO_HEAD, membership.getRelationshipToHead());
+
+            return contentValues;
+        }
+
+        @Override
+        public String getId(Membership membership) {
+            return membership.getIndividualExtId();
+        }
+    }
+}
---
+++ instrumentation-tests/src/org/openhds/mobile/tests/MembershipGatewayTest.java
@@ -0,0 +1,26 @@
+package org.openhds.mobile.tests;
+
+import org.openhds.mobile.model.Membership;
+import org.openhds.mobile.repository.MembershipGateway;
+
+
+public class MembershipGatewayTest extends GatewayTest<Membership> {
+
+    private MembershipGateway membershipGateway;
+
+    public MembershipGatewayTest() {
+        super(new MembershipGateway());
+        this.membershipGateway = (MembershipGateway) this.gateway;
+    }
+
+    @Override
+    protected Membership makeTestEntity(String id, String name) {
+        Membership membership = new Membership();
+
+        membership.setIndividualExtId(id);
+        membership.setSocialGroupExtId("SOCIALGROUP");
+        membership.setRelationshipToHead("RELATIONSHIPTOHEAD");
+
+        return membership;
+    }
+}
