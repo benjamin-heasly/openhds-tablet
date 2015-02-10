8134a457125c73e3e761e915b7132a8d5323b5f2
Andrew Taylor <andrew.dorman.taylor@gmail.com>
2015-02-10 08:28:03 -0500

    HttpTask now relies on an HttpURLConnection to make requests, taking advantage of built-in HTTPS capabilities. Recommended for Android > 2.3

--- application/src/main/java/org/openhds/mobile/task/http/HttpTask.java
+++ application/src/main/java/org/openhds/mobile/task/http/HttpTask.java
@@ -1,6 +1,7 @@
 package org.openhds.mobile.task.http;
 
 import android.os.AsyncTask;
+import android.util.Base64;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -16,8 +17,7 @@ import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.net.*;
 
 /**
  * Carry out an HttpTaskRequest.
@@ -30,6 +30,7 @@ public class HttpTask extends AsyncTask<HttpTaskRequest, Void, HttpTaskResponse>
     public static final String MESSAGE_SUCCESS = "Request successful";
     public static final String MESSAGE_NO_REQUEST = "No request given";
     public static final String MESSAGE_CLIENT_ERROR = "Client error";
+    public static final String MESSAGE_BAD_URL = "Bad URL";
     public static final String MESSAGE_SERVER_ERROR = "Server error";
 
     private HttpTaskResponseHandler httpTaskResponseHandler;
@@ -39,61 +40,44 @@ public class HttpTask extends AsyncTask<HttpTaskRequest, Void, HttpTaskResponse>
         this.httpTaskResponseHandler = httpTaskResponseHandler;
     }
 
-    // Http GET with the given url and credentials.
+    /*
+        HTTP requests are now issued by HttpURLConnection, the recommended method for android > 2.3
+        URLs with the 'https' scheme return the HttpsURLConnection subclass automatically.
+     */
     @Override
     protected HttpTaskResponse doInBackground(HttpTaskRequest... httpTaskRequests) {
         if (null == httpTaskRequests || 0 == httpTaskRequests.length) {
             return new HttpTaskResponse(false, MESSAGE_NO_REQUEST, 0, null);
         }
-        HttpTaskRequest httpTaskRequest = httpTaskRequests[0];
+        final HttpTaskRequest httpTaskRequest = httpTaskRequests[0];
 
-        // build an http client with url and credentials
-        DefaultHttpClient httpClient = new DefaultHttpClient();
-        setHttpClientCredentials(httpClient, httpTaskRequest.getUserName(), httpTaskRequest.getPassword());
+        String rawCredentials = httpTaskRequest.getUserName()+":"+httpTaskRequest.getPassword();
+        String basicAuthHeader = "Basic "+ Base64.encodeToString(rawCredentials.getBytes(), Base64.DEFAULT);
 
-        // execute a GET request from the client
+        HttpURLConnection urlConnection;
+        InputStream responseStream;
+        int statusCode;
         try {
-            HttpResponse httpResponse = executeHttpGet(httpClient, httpTaskRequest.getUrl());
-            InputStream responseStream = httpResponse.getEntity().getContent();
-            int statusCode = httpResponse.getStatusLine().getStatusCode();
-
-            if (HttpStatus.SC_OK == statusCode) {
-                return new HttpTaskResponse(true, MESSAGE_SUCCESS, statusCode, responseStream);
-            }
-
-            if (statusCode < HttpStatus.SC_INTERNAL_SERVER_ERROR) {
-                return new HttpTaskResponse(false, MESSAGE_CLIENT_ERROR, statusCode, responseStream);
-            }
-
-            return new HttpTaskResponse(false, MESSAGE_SERVER_ERROR, statusCode, responseStream);
-
+            URL url = new URL(httpTaskRequest.getUrl());
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestProperty("Authorization", basicAuthHeader);
+            responseStream = urlConnection.getInputStream();
+            statusCode = urlConnection.getResponseCode();
         } catch (Exception e) {
-            return new HttpTaskResponse(false, e.getClass().getName() +": " + e.getMessage(), 0, null);
+            return new HttpTaskResponse(false, MESSAGE_BAD_URL, 0, null);
         }
-    }
 
-    // Add credentials to this http client.
-    private void setHttpClientCredentials(DefaultHttpClient httpClient, String userName, String password) {
-        AuthScope scope = new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT, AuthScope.ANY_REALM);
-        UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(userName, password);
-        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(scope, credentials);
-        httpClient.setCredentialsProvider(credentialsProvider);
-    }
+        if (HttpStatus.SC_OK == statusCode) {
+            return new HttpTaskResponse(true, MESSAGE_SUCCESS, statusCode, responseStream);
+        }
 
-    // Make a request with preemptive basic credentials.
-    private HttpResponse executeHttpGet(DefaultHttpClient httpClient, String hostAddress)
-            throws  IOException, AuthenticationException, URISyntaxException {
+        if (statusCode < HttpStatus.SC_INTERNAL_SERVER_ERROR) {
+            return new HttpTaskResponse(false, MESSAGE_CLIENT_ERROR, statusCode, responseStream);
+        }
 
-        URL getUrl = new URL(hostAddress);
-        HttpGet httpGet = new HttpGet(getUrl.toURI());
-        HttpHost httpHost = new HttpHost(getUrl.getHost(), getUrl.getPort());
+        return new HttpTaskResponse(false, MESSAGE_SERVER_ERROR, statusCode, responseStream);
 
-        // preemptive basic credentials
-        Credentials credentials = httpClient.getCredentialsProvider().getCredentials(AuthScope.ANY);
-        httpGet.addHeader(new BasicScheme().authenticate(credentials, httpGet));
 
-        return httpClient.execute(httpHost, httpGet);
     }
 
     // Forward the Http response to the handler.
@@ -104,7 +88,7 @@ public class HttpTask extends AsyncTask<HttpTaskRequest, Void, HttpTaskResponse>
         }
     }
 
-    // A handler type to receive reponse status code and response body input stream.
+    // A handler type to receive response status code and response body input stream.
     public interface HttpTaskResponseHandler {
         public void handleHttpTaskResponse(HttpTaskResponse httpTaskResponse);
     }
