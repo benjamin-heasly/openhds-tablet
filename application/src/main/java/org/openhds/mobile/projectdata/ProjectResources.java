0da9ca0d6b8d4408ce03b895d2c848b713052abd
Andrew Taylor <andrew.d.taylor@maine.edu>
2015-06-12 13:49:35 -0400

    Strings for spray form in the form instance list

--- application/src/main/java/org/openhds/mobile/projectdata/ProjectResources.java
+++ application/src/main/java/org/openhds/mobile/projectdata/ProjectResources.java
@@ -199,6 +199,7 @@ public class ProjectResources {
         private static final String FORM_TYPE_PREGNANCY_OBSERVATION = "pregnancy_observation";
         private static final String FORM_TYPE_PREGNANCY_OUTCOME = "pregnancy_outcome";
         private static final String FORM_TYPE_LOCATION_EVALUATION = "location_evaluation";
+        private static final String FORM_TYPE_HOUSEHOLD_SPRAYING = "spraying";
 
         static {
             FormType.put(FORM_TYPE_INDIVIDUAL, R.string.form_type_individual);
@@ -211,6 +212,7 @@ public class ProjectResources {
             FormType.put(FORM_TYPE_PREGNANCY_OBSERVATION, R.string.form_type_pregnancy_observation);
             FormType.put(FORM_TYPE_PREGNANCY_OUTCOME, R.string.form_type_pregnancy_outcome);
             FormType.put(FORM_TYPE_LOCATION_EVALUATION, R.string.form_type_location_evaluation);
+            FormType.put(FORM_TYPE_HOUSEHOLD_SPRAYING, R.string.form_type_household_spraying);
         }
 
         public static int getFormTypeStringId(String key) {
